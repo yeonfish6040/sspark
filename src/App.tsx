50c041b9ddfcee81972484d98e69f79d609f2025
yeonfish6040 <yeonfish6040@gmail.com>
2026-06-09 10:37:27 +0900

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
 import { useState } from "react";
 import "./index.css"
+import axios from "axios";
 
 function App() {
+  const client = axios.create({
+    url: "http://yeonfishvm.local:3001/",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   const [num, setNum] = useState<string>("");
-  const [name, setName] = useState<string>()
+  const [name, setName] = useState<string>();
+  const [message, setMessage] = useState<string>();
+
+  const save = async () => {
+    setMessage("기다려");
+    try {
+      await client.post("/save", { num, name });
+      setMessage("성공")
+    } catch (e) {
+      console.log(e);
+      setMessage("되겠냐 ㅋ");
+    }
+  }
+
   return (
     <div>
-      <h1>
-        <div>
-          <label htmlFor="number">학번: </label>
-          <input type="text" name="number" id="number" onChange={(e) => {setNum((e.target as HTMLInputElement).value)}}/>
-        </div>
-        <div>
-          <label htmlFor="name">이름: </label>
-          <input type="text" name="name" id="name" onChange={(e) => {setName((e.target as HTMLInputElement).value)}}/>
-        </div>
-      </h1>
+      <div>
+        <label htmlFor="number">학번: </label>
+        <input type="text" name="number" id="number" onChange={(e) => {setNum((e.target as HTMLInputElement).value)}}/>
+      </div>
+      <div>
+        <label htmlFor="name">이름: </label>
+        <input type="text" name="name" id="name" onChange={(e) => {setName((e.target as HTMLInputElement).value)}}/>
+      </div>
+      <div>
+        <button onClick={() => save()}>전송</button>
+      </div>
+      <h3>{message}</h3>
     </div>
   );
 }
