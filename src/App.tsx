87a31e19f4dbf31a819b1b601451de65caf34d21
yeonfish6040 <yeonfish6040@gmail.com>
2026-07-16 12:21:23 +0900

    펑

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="page-shell">
       <main className="app-shell">
-        hello 3420 이연준
+        <h1>hello 3420 이연준</h1>
       </main>
     </div>
   );
