59e77c24c7c6fb50f8a4feb0a47b4a04a117b47b
yeonfish6040 <yeonfish6040@gmail.com>
2026-07-16 12:23:06 +0900

    펑

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="page-shell">
-      <main className="app-shell">
-        <h1>hello 3420 이연준</h1>
-      </main>
+    <div style={{textAlign: "center"}} className="page-shell">
+      <h1>hello 3420 이연준</h1>
     </div>
   );
 }
